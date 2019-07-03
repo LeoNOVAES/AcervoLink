e2b7683d09c4d69898e8e7b9d061cff1206f3016
Leandro <leandrofernandesnovaes@hotmail.com>
2019-07-03 14:42:35 -0300

    video

--- client/src/Videos.vue
+++ client/src/Videos.vue
@@ -1,17 +1,21 @@
 <template>
     <div>
-     <Header/>
-     videos
+     <Header style="margin-bottom: 5%" />
+     <div class="container">
+         <List />
+     </div>
     </div> 
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import List from "@/components/ListVideos.vue";
 import Feed from "@/components/Feed.vue";
 export default {
      components:{
         Header,
-        Feed
+        Feed,
+        List
     }
 }
 </script>
---
+++ client/src/components/ListVideos.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+    <b-card bg-variant="light" text-variant="dark">
+        <b-card-text>
+           	<form enctype="multipart/form-data">
+                <div class="custom-file">
+                    <input type="file" @change="insertFile" ref="f" class="custom-file-input" id="customFile">
+                    <label class="custom-file-label" for="customFile">Add Videos (Privados)</label>
+                </div>
+                <div class="form-group">
+                    <input type="text"  class="form-control" id="titulo" placeholder="Titulo">
+                </div>
+                <b-form-textarea
+                    maxlength="200"
+                    id="textarea"
+                    placeholder="Escreva uma descrição para a foto! (OPCIONAL)"
+                    rows="3"
+                    max-rows="6"
+                ></b-form-textarea>
+                <button class="btn btn-success">Adicionar</button>
+			</form>
+        </b-card-text>
+    </b-card>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            file:''
+        }
+    },
+    methods:{
+        insertFile(){
+            const size =  this.$refs.f.files[0].size;
+            if(size > 10048576){
+                this.$swal({
+                    title:"Arquivo de no maximo 10mb",
+                    type: 'warning',
+                    confirmButtonColor: '#41b882'
+                })
+                return;
+            }
+            console.log("pode")
+            this.$data.file = this.$refs.f.files[0];
+        }
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
